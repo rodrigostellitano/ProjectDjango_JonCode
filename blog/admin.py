cab939d9a551df1a36f15c45b5fcdb939bb0c8db
Rodrigo Stellitano <rodrigostellitano@gmail.com>
2020-07-18 02:34:13 -0300

    Second Commit

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,32 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title','author','published','status')
+    
+    list_filter = ('status','created','published','author')
+    
+    raw_id_fields = ('author',)
+    
+    date_hierarchy = 'published'
+    
+    search_fields = ('title','content')
+    
+    prepopulated_fields = {'slug':('title',)}
+
 
 # Register your models here.
+
+#   title = models.CharField(max_length=250)
+#     slug  = models.CharField(max_length=250)
+#     author = models.ForeignKey(User,
+#                                 on_delete=models.CASCADE)
+#     content = models.TextField()
+#     published = models.DateTimeField(default=timezone.now)
+#     created = models.DateTimeField(auto_now_add=timezone.now)
+#     changed = models.DateTimeField(auto_now=True)
+#     status = models.CharField(max_length=10,
+#                                 choices=STATUS,
+#                                 default='rascunho')
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset()\
+                                           .filter(status='publicado')
+
+
+
 class Post(models.Model):
     STATUS = (
         ('rascunho', 'Rascunho'),
@@ -16,14 +25,20 @@ class Post(models.Model):
                                 on_delete=models.CASCADE)
     content = models.TextField()
     published = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
     changed = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
                                 choices=STATUS,
                                 default='rascunho')
 
+
+    objects   = models.Manager()
+    published_ = PublishedManager()
+
+
     class Meta:
-        ordering = ('title',)
+        ordering = ('published',)
+       
 
     def __str__(self):
         #return self.title
