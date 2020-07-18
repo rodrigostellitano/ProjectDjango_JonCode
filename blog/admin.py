3e999b9f2a667eb5eb80e413269530998a3ea4e6
Rodrigo Stellitano <rodrigostellitano@gmail.com>
2020-07-18 14:19:26 -0300

    Third Commit

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 
 
+    #Cria filtros no admin
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 
@@ -18,16 +19,3 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
 
 
-# Register your models here.
-
-#   title = models.CharField(max_length=250)
-#     slug  = models.CharField(max_length=250)
-#     author = models.ForeignKey(User,
-#                                 on_delete=models.CASCADE)
-#     content = models.TextField()
-#     published = models.DateTimeField(default=timezone.now)
-#     created = models.DateTimeField(auto_now_add=timezone.now)
-#     changed = models.DateTimeField(auto_now=True)
-#     status = models.CharField(max_length=10,
-#                                 choices=STATUS,
-#                                 default='rascunho')
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -117,5 +117,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    'statics'
+]
 
 STATIC_URL = '/static/'
