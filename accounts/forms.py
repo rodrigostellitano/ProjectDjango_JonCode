622a1e2f3da2c6dbacaa6fd1123079c343fd1d55
Rodrigo Stellitano <rodrigostellitano@gmail.com>
2020-07-23 01:32:11 -0300

    Eighth Commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,6 +14,6 @@ class UserCreationFormWithEmail(UserCreationForm):
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Esté e-mail já existe, favor utilizar outro")
+            raise forms.ValidationError("Esté e-mail já existe, favor utilizar outro!")
         return email
 
